16a9e72f94b52dc44a9bf6c6fa4b368a6b29ede2
FrostllX <frost3333@yandex.ru>
2025-09-28 21:09:21 +0300

    Fixed comments v2

--- tests/article.spec.js
+++ tests/article.spec.js
@@ -1,6 +1,6 @@
 import { test, expect } from '@playwright/test';
 import { faker } from '@faker-js/faker';
-import { MainPage, RegisterPage, AddArticlePage, ArticlePage, EditArticlePage  } from '../src/pages/index';
+import { MainPage, RegisterPage, ArticlePage } from '../src/pages/index';
 
 const URL = 'https://realworld.qa.guru/';
 
@@ -26,13 +26,12 @@ test.describe('Статьи пользователя', () => {
 
         const mainPage = new MainPage(page);
         const registerPage = new RegisterPage(page);
-        const addArticlePage = new AddArticlePage(page);
         const articlePage = new ArticlePage(page);
 
         await mainPage.gotoRegister();
         await registerPage.register(user);
         await mainPage.gotoArticle();
-        await addArticlePage.addArticle(article);
+        await articlePage.addArticle(article);
         await expect (articlePage.articleTitle).toContainText(article.title);
         await expect (articlePage.articleAuthor).toContainText(user.name);
         await expect (articlePage.articleBody).toContainText(article.body);
@@ -62,9 +61,9 @@ test.describe('Статьи пользователя', () => {
 
         const mainPage = new MainPage(page);
         const registerPage = new RegisterPage(page);
-        const addArticlePage = new AddArticlePage(page);
+        const addArticlePage = new ArticlePage(page);
         const articlePage = new ArticlePage(page);
-        const editArticlePage = new EditArticlePage(page);
+        const editArticlePage = new ArticlePage(page);
 
         await mainPage.gotoRegister();
         await registerPage.register(user);
@@ -105,7 +104,7 @@ test.describe('Статьи пользователя', () => {
 
         const mainPage = new MainPage(page);
         const registerPage = new RegisterPage(page);
-        const addArticlePage = new AddArticlePage(page);
+        const addArticlePage = new ArticlePage(page);
         const articlePage = new ArticlePage(page);
 
         await mainPage.gotoRegister();
